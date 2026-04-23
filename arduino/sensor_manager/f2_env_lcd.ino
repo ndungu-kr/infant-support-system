32656e48f21e7997246514540c06a0bce47489e5
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2026-04-23 15:09:25 +0100

    feature 2 comments

--- arduino/sensor_manager/f2_env_lcd.ino
+++ arduino/sensor_manager/f2_env_lcd.ino
@@ -1,25 +1,49 @@
+// ============================================
+// FEATURE 2 - Environmental Monitoring + LCD
+// ============================================
+// Components: Temperature/Humidity Sensor (DHT11 or DHT22)
+//             Light Sensor
+//             LCD Backlight Display
+// Purpose:    Read environment data, show temp/humidity on LCD,
+//             send all readings to the Pi for analytics.
+// ============================================
+
 #include <DHT.h>
 
-const int DHT_PIN = 3;
-const int LIGHT_PIN = A0;
+const int DHT_PIN = 3;             // Digital pin for DHT sensor
+const int LIGHT_PIN = A0;          // Analog pin for light sensor
 DHT dht(DHT_PIN, DHT11); 
 
+// TODO: Add LCD library include and setup
+
+
 void setupEnvSensors() {
+  pinMode(LIGHT_PIN, INPUT);
+
+  // Initialise DHT sensor
   dht.begin();
+
+  // TODO: Initialise LCD
+
 }
 
 float readTemperature() {
-  float t = dht.readTemperature();
-  if (isnan(t)) return -1;
-  return t;
+  float temp = dht.readTemperature();
+  if (isnan(temp)) return -1;
+  return temp;
 }
 
 float readHumidity() {
-  float h = dht.readHumidity();
-  if (isnan(h)) return -1;
-  return h;
+  float humidity = dht.readHumidity();
+  if (isnan(humidity)) return -1;
+  return humidity;
 }
 
 int readLight() {
   return analogRead(LIGHT_PIN);
+}
+
+void updateLCD(float temp, float humidity) {
+  // TODO: Display temperature and humidity on the LCD
+
 }
